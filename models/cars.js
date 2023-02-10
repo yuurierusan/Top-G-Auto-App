d8ebe6e60bff36c828a465311e73a0a653b90448
Jonshea Nutson <yuurieru@icloud.com>
2023-02-10 09:46:25 -0500

    created cars schema

---
+++ models/cars.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const Cars = new Schema(
+    {
+        image: { type: String, required: false },
+        owner: { type: String, required: true },
+        make: { type: String, required: true },
+        model: { type: String, required: true },
+        year: { type: Number, required: false },
+    },
+    { timestamps: true }
+)
+
+module.exports = mongoose.model('Cars', Cars)
