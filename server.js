007d2a37c32f0ee32a2043d0a86a7720d1104bdc
Jonshea Nutson <yuurieru@icloud.com>
2023-02-10 16:14:00 -0500

    imported browser router to main.jsx

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const routes = require('./routes')
 const db = require('./db')
 const logger = require('morgan')
-const cors = require('./cors')
+const cors = require('cors')
 // require() imports and middleware here ^ ///////
 
 const PORT = process.env.PORT || 3001
