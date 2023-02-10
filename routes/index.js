f5068cf37203214405beda8cc36c415e31dc7e4a
Jonshea Nutson <yuurieru@icloud.com>
2023-02-10 14:22:17 -0500

    updated index

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,15 @@ const carController = require('../controllers/cars')
 const router = Router()
 
 router.get('/', (req, res) => res.send('This is Groot!'))
+
 router.post('/cars', carController.createCar)
+
 router.get('/cars', carController.getAllCars)
-router.get('/cars/:id', carController.getCarById)router.put('/cars/:id', carController.updateCar)
+
+router.get('/cars/:id', carController.getCarById)
+
+router.put('/cars/:id', carController.updateCar)
+
 router.delete('/cars/:id', carController.deleteCar)
+
 module.exports = router
