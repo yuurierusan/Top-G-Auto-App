3e7e1b4a0a653a4893bf917c197a6fd1b921e3ae
Jonshea Nutson <yuurieru@icloud.com>
2023-02-10 12:10:08 -0500

    create controller for creating car

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const routes = require('./routes')
 const db = require('./db')
+const cors = require('./cors')
 
 // require() imports and middleware here ^ ///////
 
