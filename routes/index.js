5a33f5a8494abf95b08d5820b080dcf62898e814
Jonshea Nutson <yuurieru@icloud.com>
2023-02-10 14:02:49 -0500

    updated car controller

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const carController = require('../controllers/cars')
 const router = Router()
 
 router.get('/', (req, res) => res.send('Deez Nuts!'))
-
 router.post('/cars', carController.createCar)
+router.get('/cars', carController.getAllCars)
 
 module.exports = router
