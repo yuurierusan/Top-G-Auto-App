aed00d596cad1f207d15426c20ef1adfa476b7aa
Jonshea Nutson <yuurieru@icloud.com>
2023-02-10 12:22:38 -0500

    created comment and project controllers

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const { Router } = require('express')
+const controllers = require('./controllers')
 const router = Router()
 
 router.get('/', (req, res) => res.send('Deez Nuts!'))
 
+router.post('/cars', controllers.createCar)
 module.exports = router
