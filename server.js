54fb6f3cbae12eccf11b6cb320a70958fd53b330
Jonshea Nutson <yuurieru@icloud.com>
2023-02-10 15:44:33 -0500

    updated server js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const routes = require('./routes')
 const db = require('./db')
 const logger = require('morgan')
+const cors = require('./cors')
 // require() imports and middleware here ^ ///////
 
 const PORT = process.env.PORT || 3001
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 3001
 const app = express()
 app.use(express.json())
 app.use(logger('dev'))
+app.use(cors())
 // app.use() middleware here ^ ///////////////////
 
 app.use('/', routes)
