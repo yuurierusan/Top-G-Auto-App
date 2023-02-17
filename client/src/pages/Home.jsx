50c5833378dcdb4066d6de90882a5f386bc18618
Jonshea Nutson <yuurieru@icloud.com>
2023-02-17 16:26:40 -0500

    error fix

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import '../styles/home.css'
+import '../styles/Home.css'
 import { Link } from 'react-router-dom'
 import Header from '../components/Header'
 import ButtonToCars from '../components/ButtonToCars'
