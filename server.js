f456b6d91a3ecf794c9e3bb4ea8e5ec6acf53cd0
Jonshea Nutson <yuurieru@icloud.com>
2023-02-17 16:24:20 -0500

    heroku deploy

--- server.js
+++ server.js
@@ -10,14 +10,14 @@ const app = express()
 app.use(express.json())
 app.use(logger('dev'))
 app.use(cors())
-app.use(express.static(`${__dirname}/client/build`))
+app.use(express.static(`${__dirname}/client/dist`))
 
 app.use('/app', routes)
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.get('/*', (req, res) => {
-    res.sendFile(`${__dirname}/client/build/index.html`)
+    res.sendFile(`${__dirname}/client/dist/index.html`)
 })
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
